7a3f6d2fd1ff56ef33f1ff7ab6bbe92e965f4af2
hamidreza <hrfarzadeh@gmail.com>
2021-01-13 01:21:17 +0000

    post message

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,33 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { response } = require("express");
 
 const app = express();
 
-app.use(express.json());                                                
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
 
-app.get("/", (req, res) => {
-      res.send("hello world!!!")
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors()); 
+const arr = [];
+                                              
+
+app.get("/", (request, response) => {
+      response.json(arr)
+})
+app.post("/", (request,response) => {
+      const msg = request.body;
+      arr.push(msg)
+      console.log(arr)
+      response.json(msg)
+      
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`listening on port ${port}...`))                                                                                                                       
