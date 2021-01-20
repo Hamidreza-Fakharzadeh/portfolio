5ec00f0650008bc13ab04b8a651d4ddc7d851ca3
hamidreza <hrfarzadeh@gmail.com>
2021-01-20 12:08:21 +0000

    deploy to aws ec2 instance test

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,17 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const { response } = require("express");
 
+
+const transporter = nodemailer.createTransport({
+      host: "localhost:8080",
+      port: 587,
+      auth: {
+            user: "hrfarzadeh@gmail.com",
+            pass: process.env.PASS
+      },
+      rejectUnauthorized:false
+})
+
 const app = express();
 
 app.engine("handlebars", exphbs());
@@ -14,7 +25,8 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors()); 
-const arr = [];
+const arr = [{test: "just a test for Aws ec2 instance"}];
+
                                               
 
 app.get("/", (request, response) => {
@@ -23,6 +35,20 @@ app.get("/", (request, response) => {
 app.post("/", (request,response) => {
       const msg = request.body;
       arr.push(msg)
+      let mailOptions = {
+            from: "hrfarzadeh@gmail.com",
+            to: "hamidrezaf262@gmail.com",
+            subject: "Notification From QueueAndA",
+            text: request.body.message,
+            html: `<div>${request.body.message}<h1>QueueAndA</h1><br><h3>Hi</h3><h3>Your question is: </h3><h2>Hi</h2><h3>You can follow the link and get answer. </h3></div>`,
+          };
+      transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log("Email sent: " + info.response);
+            }
+          });
       console.log(arr)
       response.json(msg)
       
