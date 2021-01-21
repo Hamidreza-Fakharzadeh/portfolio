2f78f4457b46e38127dd8b60beca354073873824
hamidreza <hrfarzadeh@gmail.com>
2021-01-21 20:37:04 +0000

    init s3 aws

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Contact() {
+	const initialStateMsg = {
+		name: '',
+		email: '',
+		subject: ''
+	};
+	const [ msgForm, setMsgForm ] = useState(initialStateMsg);
+
+	const inputHandler = (e) => {
+		setMsgForm({ ...msgForm, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		axios.post('http://localhost:8080', msgForm).then((res) => console.log(res.data));
+		setMsgForm(initialStateMsg);
+	};
+
 	return (
 		<div className="container contact-cont">
 			<div className="row">
@@ -8,16 +26,38 @@ function Contact() {
 					<p>CONTACT WITH ME</p>
 				</div>
 				<div className="form-cont xl-col-9 lg-col-9 md-col-9 sm-col-6 col-6">
-					<form>
-						<label for="fname">Name</label>
-						<input type="text" id="fname" name="firstname" placeholder="Your name.." />
+					<form onSubmit={handleSubmit}>
+						<label htmlFor="fname">Name</label>
+						<input
+							type="text"
+							id="fname"
+							name="name"
+							value={msgForm.name}
+							onChange={inputHandler}
+							placeholder="Your name.."
+						/>
 
-						<label for="aemail">Email</label>
-						<input type="text" id="aemail" name="email" placeholder="Your Email..." />
-						<label for="subject">Subject</label>
-						<textarea id="subject" name="subject" placeholder="Please write your message..." style={{height:"200px"}} />
-
-						<input type="submit" value="Submit" />
+						<label htmlFor="email">Email</label>
+						<input
+							type="text"
+							id="email"
+							name="email"
+							value={msgForm.email}
+							onChange={inputHandler}
+							placeholder="Your Email..."
+						/>
+						<label htmlFor="subject">Subject</label>
+						<textarea
+							id="subject"
+							name="subject"
+							value={msgForm.subject}
+							placeholder="Please write your message..."
+							style={{ height: '200px' }}
+							onChange={inputHandler}
+						/>
+						<button type="submit" value="Submit">
+							Submit
+						</button>
 					</form>
 				</div>
 			</div>
